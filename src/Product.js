9bb046f3f92479897088c733160a0a403f1c6a9a
Likith123 <likithadusumalli@gmail.com>
2022-06-07 00:28:49 +0530

    HomeImahe updated

---
+++ src/Product.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Product() {
+  return (
+    <div className = "product">
+        <div className = "product-info"></div>
+
+    </div>
+  )
+}
+
+export default Product
