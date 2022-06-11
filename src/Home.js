1086501163fde670df1e12d26a540bc513e46c5a
Likith123 <likithadusumalli@gmail.com>
2022-06-12 01:52:22 +0530

    changes

--- src/Home.js
+++ src/Home.js
@@ -7,10 +7,10 @@ function Home() {
         <div className = "home-container">
             <img src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" alt="Amazon Home"  className = "home-image"/>
             <div className="home-row">
-              <Product text="The lean startup"/>
-              <Product text="The lean startup"/>
-              <Product text="The lean startup"/>
-              <Product text="The lean startup"/>
+              <Product title="The lean startup" image ="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" price={19.99} rating={5}/>
+              <Product title="Ironman Image" image ="https://i.pinimg.com/originals/84/10/ea/8410ea80c6646aad1df861362f30744e.jpg" price={19.99} rating={5}/>
+              <Product title="Doctor Strange Image" image ="https://i.annihil.us/u/prod/marvel/html_blocks_assets/doctor-strange/d-strange-poster.jpg" price={19.99} rating={5}/>
+              <Product title="The lean startup" image ="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" price={19.99} rating={5}/>
             </div>
             <div className="home-row">
                 
