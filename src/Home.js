c11a682866c24b0a1f44f8a17feee3706d856240
Likith123 <likithadusumalli@gmail.com>
2022-06-13 21:46:53 +0530

    changes

--- src/Home.js
+++ src/Home.js
@@ -9,11 +9,14 @@ function Home() {
             <div className="home-row">
               <Product title="The lean startup" image ="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" price={19.99} rating={5}/>
               <Product title="Ironman Image" image ="https://i.pinimg.com/originals/84/10/ea/8410ea80c6646aad1df861362f30744e.jpg" price={29.99} rating={5}/>
-              <Product title="Doctor Strange Image" image ="https://i.annihil.us/u/prod/marvel/html_blocks_assets/doctor-strange/d-strange-poster.jpg" price={19.99} rating={5}/>
-              <Product title="The lean startup" image ="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" price={19.99} rating={5}/>
+              <Product title="DoctorStrange" image ="https://i.annihil.us/u/prod/marvel/html_blocks_assets/doctor-strange/d-strange-poster.jpg" price={19.99} rating={5}/>
+ 
             </div>
             <div className="home-row">
-                
+                <Product title="The lean startup" image ="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" price={19.99} rating={5}/>
+              <Product title="Ironman Image" image ="https://i.pinimg.com/originals/84/10/ea/8410ea80c6646aad1df861362f30744e.jpg" price={29.99} rating={5}/>
+              <Product title="DoctorStrange" image ="https://i.annihil.us/u/prod/marvel/html_blocks_assets/doctor-strange/d-strange-poster.jpg" price={19.99} rating={5}/>
+              <Product title="The lean startup" image ="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" price={19.99} rating={5}/>
             </div>
             <div className="home-row">
                 
